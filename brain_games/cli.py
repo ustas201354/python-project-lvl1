c7e8be6b6824cc74f5c0790eb979ce25bb8aae3c
Oleg <ustas201354@gmail.com>
2021-08-10 14:42:15 +0300

    10.08.21

---
+++ brain_games/cli.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+
+
+def welcome_user():
+    name = input('May I have your name? ')
+    print('Hello', name)
+
+welcome_user()
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 def main():
+    from brain_games.cli import welcome_user
     print('Welcome to brain games')
+    welcome_user()
 
 if __name__ == '__main__':
     main()
