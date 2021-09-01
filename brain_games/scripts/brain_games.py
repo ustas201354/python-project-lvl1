190b78e33e8b4aac19cc8f23682173493183c9ee
ustas <ustas201354@gmail.com>
2021-09-01 06:52:38 +0300

    home commit 31.08.21

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,16 +1,13 @@
 #! /usr/bin/env python3
 
 import sys
-
-
 sys.path.append('../')
 from cli import welcome_user
 
 
 def main():
-
-
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
